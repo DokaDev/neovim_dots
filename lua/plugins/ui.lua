0728933dacafb896d2aa63d60cef0e7c2dcc91f5
Awesome <awesome_devnet@outlook.com>
2024-05-02 10:45:49 +0900

    update settings

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,123 @@
+return {
+    {
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		opts = function(_, opts)
+			local logo = [[
+▓█████▄  ▒█████   ██ ▄█▀▄▄▄      ▓█████▄ ▓█████ ██▒   █▓
+▒██▀ ██▌▒██▒  ██▒ ██▄█▒▒████▄    ▒██▀ ██▌▓█   ▀▓██░   █▒
+░██   █▌▒██░  ██▒▓███▄░▒██  ▀█▄  ░██   █▌▒███   ▓██  █▒░
+░▓█▄   ▌▒██   ██░▓██ █▄░██▄▄▄▄██ ░▓█▄   ▌▒▓█  ▄  ▒██ █░░
+░▒████▓ ░ ████▓▒░▒██▒ █▄▓█   ▓██▒░▒████▓ ░▒████▒  ▒▀█░  
+ ▒▒▓  ▒ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒▒▒   ▓▒█░ ▒▒▓  ▒ ░░ ▒░ ░  ░ ▐░  
+ ░ ▒  ▒   ░ ▒ ▒░ ░ ░▒ ▒░ ▒   ▒▒ ░ ░ ▒  ▒  ░ ░  ░  ░ ░░  
+ ░ ░  ░ ░ ░ ░ ▒  ░ ░░ ░  ░   ▒    ░ ░  ░    ░       ░░  
+   ░        ░ ░  ░  ░        ░  ░   ░       ░  ░     ░  
+ ░                                ░                 ░   
+      ]]
+
+			logo = string.rep("\n", 8) .. logo .. "\n\n"
+			opts.config.header = vim.split(logo, "\n")
+		end,
+	},
+
+	-- filename
+	{
+		"b0o/incline.nvim",
+		dependencies = { "craftzdog/solarized-osaka.nvim" },
+		event = "BufReadPre",
+		priority = 1200,
+		config = function()
+			local colors = require("solarized-osaka.colors").setup()
+			require("incline").setup({
+				highlight = {
+					groups = {
+						InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+						InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+					},
+				},
+				window = { margin = { vertical = 0, horizontal = 1 } },
+				hide = {
+					cursorline = true,
+				},
+				render = function(props)
+					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+					if vim.bo[props.buf].modified then
+						filename = "[+] " .. filename
+					end
+
+					local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+					return { { icon, guifg = color }, { " " }, { filename } }
+				end,
+			})
+		end,
+	},
+
+	-- animations
+	{
+		"echasnovski/mini.animate",
+		event = "VeryLazy",
+		opts = function(_, opts)
+		opts.scroll = {
+			enable = false,
+		}
+		end,
+  	},
+
+
+
+
+	-- messages, cmdline and the popupmenu
+	{
+		"folke/noice.nvim",
+		opts = function(_, opts)
+			table.insert(opts.routes, {
+				filter = {
+					event = "notify",
+					find = "No information available",
+				},
+				opts = { skip = true },
+			})
+			local focused = true
+			vim.api.nvim_create_autocmd("FocusGained", {
+				callback = function()
+					focused = true
+				end,
+			})
+			vim.api.nvim_create_autocmd("FocusLost", {
+				callback = function()
+					focused = false
+				end,
+			})
+			table.insert(opts.routes, 1, {
+				filter = {
+					cond = function()
+						return not focused
+					end,
+				},
+				view = "notify_send",
+				opts = { stop = false },
+			})
+
+			opts.commands = {
+				all = {
+					-- options for the message history that you get with `:Noice`
+					view = "split",
+					opts = { enter = true, format = "details" },
+					filter = {},
+				},
+			}
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "markdown",
+				callback = function(event)
+					vim.schedule(function()
+						require("noice.text.markdown").keys(event.buf)
+					end)
+				end,
+			})
+
+			opts.presets.lsp_doc_border = true
+		end,
+	},
+}
