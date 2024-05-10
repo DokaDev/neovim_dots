66db76ec676f1a6ef1edc4c3dbb3cb12803313d9
Awesome <awesome_devnet@outlook.com>
2024-05-10 09:18:16 +0900

    update configs

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -64,6 +64,24 @@ return {
 		end,
   	},
 
+	-- buffer line
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+			{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+		},
+		opts = {
+			options = {
+				mode = "tabs",
+				-- separator_style = "slant",
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+			},
+		},
+	},
+
 
 
 
@@ -119,5 +137,12 @@ return {
 
 			opts.presets.lsp_doc_border = true
 		end,
+	},
+
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 5000,
+		},
 	},
 }
