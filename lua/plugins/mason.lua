7eadda30df98e96bd90229553cc1babe2149a1ee
Awesome <awesome_devnet@outlook.com>
2025-08-16 20:17:06 +0900

    mason 안정화 2

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,24 +2,22 @@
 return {
     -- mason 본체
     {
-      "williamboman/mason.nvim",
-      event = "VeryLazy",
-      opts = {
-        max_concurrent_installers = 2,          -- 동시 설치 낮춰
-        ui = { check_outdated_packages_on_open = false },
+        "williamboman/mason.nvim",
+        event = "VeryLazy",
+        opts = {
+          max_concurrent_installers = 2,  -- 기본보다 낮춰. 1~2가 제일 안정적.
+          ui = { check_outdated_packages_on_open = false },
+        },
       },
-    },
-  
-    -- LSP 매핑 (자동설치 끄고 최소만 보장)
-    {
-      "williamboman/mason-lspconfig.nvim",
-      event = "VeryLazy",
-      dependencies = { "williamboman/mason.nvim" },
-      opts = {
-        ensure_installed = { "lua_ls", "ts_ls" }, -- 진짜 쓸 것만
-        automatic_installation = false,           -- 자동 설치 금지(중요)
+      {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "williamboman/mason.nvim" },
+        opts = {
+          ensure_installed = { "lua_ls", "ts_ls" }, -- 진짜 쓰는 최소만
+          automatic_installation = true,            -- 자동설치 허용
+        },
       },
-    },
   
     -- (선택) 포매터/린터 관리까지 하고 싶으면
     -- {
