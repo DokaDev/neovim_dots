218bd83c4719a008569f1eab6c70f17a42dd32be
Awesome <awesome_devnet@outlook.com>
2025-08-16 15:55:15 +0900

    deprecated된 라인 제거

--- lua/plugins/git_signs.lua
+++ lua/plugins/git_signs.lua
@@ -2,19 +2,19 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    enabled = true,                                -- 강제 ON
-    event = { "BufReadPre", "BufNewFile" },       -- 표준 로드 타이밍
+    enabled = true, -- 강제 ON
+    event = { "BufReadPre", "BufNewFile" }, -- 표준 로드 타이밍
     opts = {
-      signcolumn = true,   -- 삭제 마커용 (사인컬럼)
-      numhl      = true,   -- 추가/변경은 줄번호 색으로
-      linehl     = false,
-      show_deleted = false, -- 기본은 숨김
+      signcolumn = true, -- 삭제 마커용 (사인컬럼)
+      numhl = true, -- 추가/변경은 줄번호 색으로
+      linehl = false,
+      -- show_deleted = false, -- 기본은 숨김
 
       signs = {
-        add          = { text = "│" },
-        change       = { text = "│" },
-        delete       = { text = "▁" },
-        topdelete    = { text = "▔" },
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "▁" },
+        topdelete = { text = "▔" },
         changedelete = { text = "┆" },
       },
 
@@ -36,3 +36,4 @@ return {
     },
   },
 }
+
