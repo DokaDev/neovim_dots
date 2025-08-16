69673e817df3d70477ccca1488e03e53545a8134
Awesome <awesome_devnet@outlook.com>
2025-08-16 21:01:26 +0900

    mason 안정화 7

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,34 +3,44 @@ return {
   {
     "mason-org/mason.nvim",
     opts = function(_, opts)
-      opts.max_concurrent_installers = 1 -- 더 줄여. 1이 제일 안전
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, {
-        "stylua",
-        "selene",
-        "luacheck",
-        "shellcheck",
-        "shfmt",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "css-lsp",
-        "prisma-language-server",
-      })
+      opts.max_concurrent_installers = 1
+      -- opts.ensure_installed = opts.ensure_installed or {}
+      -- vim.list_extend(opts.ensure_installed, {
+      --   "stylua",
+      --   "selene",
+      --   "luacheck",
+      --   "shellcheck",
+      --   "shfmt",
+      --   "tailwindcss-language-server",
+      --   "typescript-language-server",
+      --   "css-lsp",
+      --   "prisma-language-server",
+      -- })
+    end,
+  },
+  -- mason-lspconfig: 자동설치 완전히 끈다(중요)
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = {}          -- 비움 (필요해지면 최소한으로만 넣기)
+      opts.automatic_installation = false -- 부팅/attach 시 자동 설치 금지
     end,
   },
   -- mason-tool-installer: 순차 설치 + 딜레이
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
+    enabled = false,        -- <- 당분간 비활성화
     event = "VeryLazy",
     dependencies = { "mason-org/mason.nvim" },
     opts = {
-      ensure_installed = {}, -- (여기엔 툴 몇 개만 넣어도 됨)
-      run_on_start = true,
-      start_delay = 3000, -- 3초 뒤 시작
+      ensure_installed = {},
+      run_on_start = false, -- 부팅 때 돌리지 않도록 함
+      start_delay = 3000,
       debounce_hours = 12,
       auto_update = false,
     },
   },
+
   -- {
   --   "mason-org/mason.nvim",
   --   opts = {
