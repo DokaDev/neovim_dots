98d1c9ee5ff51f1c86d0830b5050d000c9182e9a
Awesome <awesome_devnet@outlook.com>
2025-08-17 17:14:01 +0900

    bufferline임시 커밋1

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,94 +4,29 @@ return {
     "akinsho/bufferline.nvim",
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
-    --
-    --   -- 뉴버전 설정
-    --   config = function()
-    --     vim.o.mousemoveevent = true
-    --
-    --     local bufferline = require("bufferline")
-    --
-    --     bufferline.setup({
-    --       highlights = {
-    --         buffer_selected = {
-    --           bold = false,
-    --           italic = false,
-    --         },
-    --       },
-    --       options = {
-    --         indicator = {
-    --           style = "underline",
-    --         },
-    --         max_name_length = 50,
-    --       },
-    --     })
-    --   end,
-
     -- 뉴버전 설정끝
     -- 아래는 원래 쓰던 버전 --
     opts = {
       options = {
         -- 색상 투명화 관련 옵션들은 config/autocmds.lua 파일에 지정되어 있음
-        -- indicator = { style = "underline" },
-        -- separator_style = "slope",
-        -- mode = "tabs",
         mode = "buffers",
-        -- always_show_bufferline = true,
-        -- indicator = { style = "underline" }, -- 선택 버퍼 밑줄 인디케이터 표시
-
         always_show_bufferline = true,
         show_tab_indicators = true,
 
         tab_size = 18,
 
-        separator_style = "thin",
-        themable = true,
+        -- separator_style = "thin",
+        -- themable = true,
 
         indicator = {
           style = "underline",
         },
-        -- hover = {
-        --   enabled = true,
-        --   delay = 200,
-        --   reveal = { "close" },
-        -- },
         buffer_close_icon = "󰅖",
         modified_icon = "● ",
         close_icon = " ",
         left_trunc_marker = " ",
         right_trunc_marker = " ",
-
-        -- offsets = {
-        --   {
-        --     text_align = "center",
-        --     separator = true,
-        --   },
-        -- },
-
-        -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
-        --   local s = " "
-        --   for e, n in pairs(diagnostics_dict) do
-        --     local sym = e == "error" and "  " or (e == "warning" and "  " or "  ")
-        --     s = s .. n .. sym
-        --   end
-        --   return s
-        -- end,
       },
-      --
-      --   ------------------------------------------------
-      --
-      --   -- highlights = {
-      --   --   tab_separator_selected = {
-      --   --     underline = true,
-      --   --     sp = "#ff5f87",
-      --   --   },
-      --   -- },
-      --   highlights = {
-      --     indicator_selected = {
-      --       sp = "#FF0000",
-      --     },
-      --   },
-      -- },
     },
   },
 }
