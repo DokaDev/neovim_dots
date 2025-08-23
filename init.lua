62ed9cc2e7b1e1bfddc0c12260da6b9b2e37fc5e
Awesome <awesome_devnet@outlook.com>
2025-08-23 10:08:42 +0900

    lualine 구성 개선 및 showcmd 키캡처 표시 추가

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 vim.o.termguicolors = true
+vim.o.showcmdloc = "statusline"
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -17,8 +17,24 @@ return {
           lualine_b = { "branch", "diff", "diagnostics" },
           lualine_c = {
             {
+              "filetype",
+              colored = true, -- Displays filetype icon in color if set to true
+              icon_only = true, -- Display only an icon for filetype
+              icon = { align = "right" }, -- Display filetype icon on the right hand side
+              -- icon =    {'X', align='right'}
+              -- Icon string ^ in table is ignored in filetype component
+            },
+            {
               "filename",
               path = 1, -- 0: 파일명만, 1: 상대경로, 2: 절대경로, 3: 절대경로+tilde, 4: 파일명(부모디렉토리 포함)
+              file_status = true,
+              newfile_status = false,
+              symbols = {
+                modified = "[+]",
+                readonly = "[-]",
+                unnamed = "[No Name]",
+                nofile = "[No File]",
+              },
             },
             "harpoon2",
           },
@@ -29,6 +45,8 @@ return {
             -- "datetime", -- 현재 시간/날짜 표시
             -- "tabs",               -- 탭 개수와 현재 탭 번호 (예: [2/4])
             -- "windows", -- 윈도우 개수와 현재 윈도우 번호 (예: 1/3)
+            "%S",
+
             {
               function()
                 return require("copilot_status").status_string()
@@ -41,7 +59,7 @@ return {
             "fileformat",
             "filetype",
           },
-          lualine_y = { "progress" },
+          lualine_y = { "tabs", "progress" },
           lualine_z = { "location" },
         },
         extensions = { "neo-tree", "lazy" },
