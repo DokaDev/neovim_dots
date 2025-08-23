80b1f03ce984c5b9a8c5919bcb8acb2316c4d9b0
Awesome <awesome_devnet@outlook.com>
2025-08-23 15:10:28 +0900

    git symbol단위로 요청하는 문제로 인해 FD 문제 발생하여 수정

--- lua/plugins/lsp_lens.lua
+++ lua/plugins/lsp_lens.lua
@@ -16,9 +16,10 @@ return {
           implements = function(count)
             return "Implementations: " .. count
           end,
-          git_authors = function(latest_author, count)
-            return " " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
-          end,
+          -- git_authors = function(latest_author, count)
+          --   return " " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
+          -- end,
+          git_authors = false,
         },
         ignore_filetype = {},
 
