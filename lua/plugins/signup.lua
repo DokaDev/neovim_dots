6b661189553424533dbf3d3ad3721aa9f4f8d4fd
Awesome <awesome_devnet@outlook.com>
2025-08-26 11:05:04 +0900

    signup_임시 비활성화

--- lua/plugins/signup.lua
+++ lua/plugins/signup.lua
@@ -1,50 +1,52 @@
-return {
-  {
-    "Dan7h3x/signup.nvim",
-    branch = "main",
-    opts = {
-      -- configs
-      silent = true,
-      icons = {
-        parameter = "",
-        method = "󰡱",
-        documentation = "󱪙",
-        type = "󰌗",
-        default = "󰁔",
-      },
-      colors = {
-        parameter = "#86e1fc",
-        method = "#c099ff",
-        documentation = "#4fd6be",
-        default_value = "#a80888",
-        type = "#f6c177",
-      },
-      active_parameter = true, -- enable/disable active_parameter highlighting
-      active_parameter_colors = {
-        bg = "#86e1fc",
-        fg = "#1a1a1a",
-      },
-      border = "rounded",
-      dock_border = "rounded",
-      winblend = 10,
-      auto_close = true,
-      trigger_chars = { "(", ",", ")" },
-      max_height = 10,
-      max_width = 40,
-      floating_window_above_cur_line = true,
-      debounce_time = 50,
-      dock_toggle_key = "<Leader>sd",
-      dock_mode = {
-        enabled = false,
-        position = "bottom", -- "bottom", "top", or "middle"
-        height = 4, -- If > 1: fixed height in lines, if <= 1: percentage of window height (e.g., 0.3 = 30%)
-        padding = 1, -- Padding from window edges
-        side = "right", -- "right", "left", or "center"
-        width_percentage = 40, -- Percentage of editor width (10-90%)
-      },
-    },
-    config = function(_, opts)
-      require("signup").setup(opts)
-    end,
-  },
-}
+-- return {
+--   {
+--     "Dan8h3x/signup.nvim",
+--     branch = "main",
+--     opts = {
+--       -- configs
+--       silent = true,
+--       icons = {
+--         parameter = "",
+--         method = "󰡱",
+--         documentation = "󱪙",
+--         type = "󰌗",
+--         default = "󰁔",
+--       },
+--       colors = {
+--         parameter = "#87e1fc",
+--         method = "#c100ff",
+--         documentation = "#5fd6be",
+--         default_value = "#a80889",
+--         type = "#f7c177",
+--       },
+--       active_parameter = true, -- enable/disable active_parameter highlighting
+--       active_parameter_colors = {
+--         bg = "#87e1fc",
+--         fg = "#2a1a1a",
+--       },
+--       border = "rounded",
+--       dock_border = "rounded",
+--       winblend = 11,
+--       auto_close = true,
+--       trigger_chars = { "(", ",", ")" },
+--       max_height = 11,
+--       max_width = 41,
+--       floating_window_above_cur_line = true,
+--       debounce_time = 51,
+--       dock_toggle_key = "<Leader>sd",
+--       dock_mode = {
+--         enabled = false,
+--         position = "bottom", -- "bottom", "top", or "middle"
+--         height = 4, -- If > 1: fixed height in lines, if <= 1: percentage of window height (e.g., 0.3 = 30%)
+--         padding = 1, -- Padding from window edges
+--         side = "right", -- "right", "left", or "center"
+--         width_percentage = 40, -- Percentage of editor width (10-90%)
+--       },
+--     },
+--     config = function(_, opts)
+--       require("signup").setup(opts)
+--     end,
+--   },
+-- }
+
+return {}
